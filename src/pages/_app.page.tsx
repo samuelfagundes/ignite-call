b106d00554bf05ac787215a849c96039be371b80
Samuel Fagundes <samuelfagundessva@gmail.com>
2023-03-30 20:21:10 -0300

    add google calendar connection

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import { globalStyles } from '@/styles/global'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
 
@@ -6,10 +7,15 @@ const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <div className={roboto.className}>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </div>
   )
 }
